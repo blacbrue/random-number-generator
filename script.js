f6256e947e4f4e4990f64acdd870e7d034e415e3
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2020-11-14 23:04:52 +0800

    NEW FILES
    ALL CAPS BECAUSE I HAVE A ZOOM GITHUB BOT SUBSCRIBED TO THIS REPOSITORY THINGA IDK HAHAHAHAHAHAHA

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 10) + 1;
+}
+
+function myFunction1() {
+    document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 100) + 1;
+}
+
+function myFunction2() {
+    document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 1000) + 1;
+}
+
+function myFunction3() {
+    document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 10000) + 1;
+}
+
+function myFunction4() {
+    document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 100000) + 1;
+}
+
+function myFunction5() {
+    document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 1000000) + 1;
+}
